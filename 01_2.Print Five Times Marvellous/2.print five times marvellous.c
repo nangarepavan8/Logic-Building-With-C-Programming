3b97e6be46db44e09e540974d7e574b3c273c971
PAVAN SAMBHAJI NANGARE-PATIL <62687233+nangarepavan8@users.noreply.github.com>
2022-01-02 09:21:36 +0530

    Print 5 times Marvellous

---
+++ 01_2.Print Five Times Marvellous/2.print five times marvellous.c
@@ -0,0 +1,18 @@
+//      print five times "marvelleous" on screen                         //
+#include<stdio.h>
+
+void Display()
+{
+    int i=0;
+    for( i=1;i<=5;i++)
+    {
+        printf("marvellous\t \n");
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
