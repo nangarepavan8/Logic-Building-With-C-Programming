9b9103d5a5328009ec18886f7a69b7a437ae3be1
PAVAN SAMBHAJI NANGARE-PATIL <62687233+nangarepavan8@users.noreply.github.com>
2022-01-03 09:48:40 +0530

    Print 5 to 1

---
+++ 01_3.Print 5 to 1/3..F.print 5 to 1.c
@@ -0,0 +1,23 @@
+//      print five times "marvelleous" on screen                         //
+#include<stdio.h>
+
+int Display()
+{
+    int i=0;
+   // int i=5;
+
+    for( i=5;i>=1;--i)
+    {
+        printf("%d \n",i);
+       // i++;
+    }
+    
+    
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
