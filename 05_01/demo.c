396561ae0cab450488a68d754fbcdc0ae008e560
PAVAN SAMBHAJI NANGARE-PATIL <62687233+nangarepavan8@users.noreply.github.com>
2022-01-17 09:53:37 +0530

    accept name from user and print that name.
    Write a program which accept name from user and print that name.

---
+++ 05_01/demo.c
@@ -0,0 +1,16 @@
+
+// write the program which accepts name from user and print that name..//
+
+#include<stdio.h>
+
+int main()
+{
+    char Name[4];
+    printf("enter your full name please\n");
+
+    scanf("%s",&Name);
+
+    printf("your full name is\n %s",Name);
+
+    return 0;
+}
